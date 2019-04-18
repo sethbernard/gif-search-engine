4acd57e53f2903f96cb00812b17ba1f337aacd05
Seth Bernard <sethbernard7@gmail.com>
2019-04-18 16:44:38 -0400

    Add Material UI and style form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
 
 class Form extends Component {
   render() {
     return (
-      <div className="form">
-        <form onSubmit={this.props.onSubmit}>
-          <input
-            placeholder="Find some GIFS"
-            onChange={this.props.onChange}
-            value={this.props.value}
-            name="text"
-          />
-          <input
-            placeholder="Number of GIFS"
-            value={this.props.value}
-            onChange={this.props.onChange}
-            name="limit"
-          />
-          <button>Click me</button>
-        </form>
-      </div>
+      <form onSubmit={e => this.props.onSubmit(e)}>
+        <TextField
+          className="input"
+          placeholder="Find some GIFS"
+          onChange={e => this.props.onChange(e)}
+          value={this.props.value}
+          name="text"
+          margin="normal"
+        />
+
+        <TextField
+          className="input"
+          placeholder="Number of GIFS"
+          value={this.props.value}
+          onChange={e => this.props.onChange(e)}
+          name="limit"
+          margin="normal"
+        />
+        <Button
+          className="btn"
+          variant="contained"
+          color="primary"
+          onClick={e => this.props.onSubmit(e)}
+          type="submit"
+        >
+          Click me
+        </Button>
+      </form>
     );
   }
 }
