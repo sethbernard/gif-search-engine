99450ebe3833e4d56754d4062d3d9f77cc1d8c84
Seth Bernard <sethbernard7@gmail.com>
2019-04-25 13:47:18 -0400

    Add Github profile link to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="footer">
-      <a href="https://github.com/sethbernard">
+      <a
+        href="https://github.com/sethbernard"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Developed by <strong>Seth Bernard</strong>
       </a>
     </div>
